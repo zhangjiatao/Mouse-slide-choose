2027050e7b6e0ca1e0eef5f715b69967fa87c96f
Jiatao Zhang <1193744197@qq.com>
2019-03-27 10:29:21 +0800

    updata code

--- src/main.py
+++ src/main.py
@@ -2,18 +2,12 @@ from pymouse import PyMouse
 from pymouse import PyMouseEvent
 from pykeyboard import PyKeyboardEvent
 from pykeyboard import PyKeyboard
-
-# m = PyMouse()
-# x_dim, y_dim = m.screen_size()
-
-# m.click(x_dim/2, y_dim/2, 1)
-
-
+'''
+通过滑动鼠标来实现多个token的选取
+'''
+TOKEN_WIDTH = 30 #小格子的宽度,需要根据屏幕尺寸调整,可以根据打印的start_postion进行设置
 
 class Clickonacci(PyMouseEvent):
-
-
-	
 
 	def __init__(self):
 		PyMouseEvent.__init__(self)
@@ -22,43 +16,30 @@ class Clickonacci(PyMouseEvent):
 		self.mouse = PyMouse()
 
 	def click(self, x, y, button, press):
-		'''Print Fibonacci numbers when the left click is pressed.'''
 		if button == 1:
 			if press:
-				print ('start:',PyMouse().position())
+				# 记录鼠标滑动起始位置
+				print ('start_postion:',PyMouse().position())
 				self.s_x, self.s_y = PyMouse().position()
 
 			else:
-				print('end:', PyMouse().position())
+				print('end_postion:', PyMouse().position())
+				# 记录终止位置
 				e_x, e_y = PyMouse().position()
-				if(abs(e_x - self.s_x) > 20):
-
+				# 屏蔽短距离移动
+				if(abs(e_x - self.s_x) > 10):
 					temp_x = min(self.s_x, e_x)
-					num = (abs(e_x - temp_x) / 30) + 1 # 计算点击次数
+					num = (abs(e_x - temp_x) / TOKEN_WIDTH) + 1 # 计算点击次数，30是小格子的宽度，需要根据屏幕尺寸调整
 					print('自动点击次数:', num)
+					# 在起始位置和终止位置之间每隔一个TOKEN_WIDTH进行一次点击
 					for i in range(int(num)):
 						PyMouse().click(temp_x, e_y, 1, 1)
-						temp_x = temp_x + 30
+						temp_x = temp_x + TOKEN_WIDTH 
 
-			# if release:
-			# 	print('over:', PyMouse().position())
-		else: # Exit if any other mouse button used
+		else:
 			self.stop()
-	# def move(self, x, y):
-	# 	print ("the mouse was moved to", x, y)
-		# return x, y
 
+if __name__ == '__main__':
+	C = Clickonacci()
+	C.run()
 
-class TapRecord(PyKeyboardEvent):
-	def __init__(self):
-		PyKeyboardEvent.__init__(self)
-
-	def tap(self, keycode, character, press):
-		print(time.time(), keycode, character, press)
-
-
-
-C = Clickonacci()
-C.run()
-# K = TapRecord()
-# K.run()
